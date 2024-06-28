d42e1d64b1eeae67a639ca92aeed576fb04c91a8
raaasin <nisarvskp@gmail.com>
2024-06-28 23:12:07 +0530

    rem

--- int.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import pandas as pd
-data = pd.Series([1, 3, 2, 4, 5],['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05'])
-plt.plot(data)
-plt.show()
