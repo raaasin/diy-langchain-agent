e5f5ac367d3ce3798fa6661e581afb3d5318205b
raaasin <nisarvskp@gmail.com>
2024-06-30 14:44:36 +0530

    bind

--- main.py
+++ main.py
@@ -89,7 +89,6 @@ prompt = ChatPromptTemplate.from_messages(
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
-
 # Bind tools to LLM
 tools = [calculate_health_star_rating]
 llm_with_tools = llm.bind_tools(tools)
